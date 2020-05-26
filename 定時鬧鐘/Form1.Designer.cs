7140935b8cfd514f30a3f963b82873aad31a1286
Rickiarty <fatalframe0719@gmail.com>
2020-05-27 07:09:03 +0800

    Add files via upload
    Initialization

---
+++ 定時鬧鐘/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace 定時鬧鐘
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(653, 396);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "停止播放音效";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(776, 363);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Message:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "排程鬧鐘：我在這裡！";
+            this.notifyIcon1.BalloonTipTitle = "scheduled alarm program";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "scheduled alarm program";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            this.notifyIcon1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scheduled Alarm - 排程鬧鐘";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ 定時鬧鐘/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Media;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 定時鬧鐘
+{
+    public partial class Form1 : Form
+    {
+        private bool isPlaying = false;
+        private bool shouldStopForAWhile = false;
+        private Thread playerThread = null;
+        private Thread scheduleThread = null;
+        private Thread blockMusicThread = null;
+        private SoundPlayer player = new SoundPlayer();
+        Dictionary<int, string> schedule = new Dictionary<int, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.readSchedule(@".\schedule.txt");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.scheduleThread = new Thread(this.monitorSchedule);
+            this.scheduleThread.Start();
+            this.notifyIcon1.Visible = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //DialogResult dResult = MessageBox.Show("確定要離開系統?", "離開系統", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            DialogResult dResult = MessageBox.Show("這樣做會導致排程鬧鐘停止運作，確定要關閉程式？", "關閉程式", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (dResult == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                this.stopPlayingMusic(false);
+                this.deinitialize();
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.notifyIcon1.Visible = true;
+                this.Hide();
+            }
+            else
+            {
+                this.notifyIcon1.Visible = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.blockMusicThread = new Thread(() => this.stopPlayingMusic(true));
+            this.blockMusicThread.Start();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void notifyIcon1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.notifyIcon1.ShowBalloonTip(3000);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.notifyIcon1.Visible = false;
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void deinitialize()
+        {
+            if (this.blockMusicThread != null)
+            {
+                this.blockMusicThread.Abort();
+                this.blockMusicThread = null;
+            }
+
+            if (this.scheduleThread != null)
+            {
+                this.scheduleThread.Abort();
+                this.scheduleThread = null;
+            }
+
+            if (this.playerThread != null)
+            {
+                this.playerThread.Abort();
+                this.playerThread = null;
+            }
+        }
+
+        async private void stopPlayingMusic(bool blockAlarm)
+        {
+            if (isPlaying)
+            {
+                if (this.player != null)
+                {
+                    this.player.Stop();
+                    if (blockAlarm)
+                    {
+                        this.shouldStopForAWhile = true;
+                        await waitForFewSeconds(59);
+                        this.shouldStopForAWhile = false;
+                    }
+                }
+                this.isPlaying = false;
+            }
+        }
+
+        async private void playMusic()
+        {
+            this.isPlaying = true;
+            this.player = new SoundPlayer();
+            this.player.SoundLocation = @".\music.wav";
+            this.player.Play();
+            await waitForFewSeconds(60);
+            this.player.Stop();
+            this.isPlaying = false;
+        }
+
+        private Task<int> waitForFewSeconds(int seconds)
+        {
+            Thread.Sleep(1000 * seconds);
+            return Task<int>.Factory.StartNew(() => seconds);
+        }
+
+        private void readSchedule(string scheduleFilePath)
+        {
+            using (StreamReader sr = new StreamReader(scheduleFilePath))
+            {
+                var text = sr.ReadToEnd();
+                text = text.Trim(' ').Trim('\n').Trim('\t');
+                var tsArr = text.Split(';');
+                var i = 0;
+                foreach (string s in tsArr)
+                {
+                    if (string.IsNullOrEmpty(s)) continue;
+                    try
+                    {
+                        var tsArr2 = s.Split('#');
+                        var comment = tsArr2[1];
+                        var tsArr3 = tsArr2[0].Split(':');
+                        var hour = int.Parse(tsArr3[0]);
+                        var minute = int.Parse(tsArr3[1]);
+                        var time = hour * 60 + minute;
+                        this.schedule[time] = comment;
+                        i += 1;
+                    }
+                    catch (Exception ex)
+                    {
+                        this.textBox1.Text += ex.ToString();
+                    }
+                }
+            }
+        }
+
+        public void monitorSchedule()
+        {
+            while (true)
+            {
+                var now = DateTime.Now;
+                var hour = now.Hour;
+                var minute = now.Minute;
+                var time = hour * 60 + minute;
+                if (this.schedule.ContainsKey(time))
+                {
+                    if (!isPlaying)
+                    {
+                        this.playerThread = new Thread(this.playMusic);
+                        this.playerThread.Start();
+                        this.textBox1.Text += this.schedule[time] + "\n";
+                    }
+                }
+                Thread.Sleep(10 * 1000);
+            }
+        }
+
+    }
+}
